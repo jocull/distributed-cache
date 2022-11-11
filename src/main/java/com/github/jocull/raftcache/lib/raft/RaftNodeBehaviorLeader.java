401bdfee494eba5dc99e19951470fbfbf3617259
James O'Cull <james.ocull@gmail.com>
2022-11-10 23:45:48 -0500

    Try to set the next heartbeat timeout based on the connection that needs it most
    There's some weird timing issues I haven't figured out in here... Tests
    will randomly show very long delays between operations, which leads to
    heartbeats that don't arrive on time and new elections taking place, and
    even split vote elections in severe cases.
    
    Could be related to GC? Unfair locking? Greedy thread pool? Bad time
    handling in scheduled thread pool? I don't know yet :(

--- src/main/java/com/github/jocull/raftcache/lib/raft/RaftNodeBehaviorLeader.java
+++ src/main/java/com/github/jocull/raftcache/lib/raft/RaftNodeBehaviorLeader.java
@@ -8,7 +8,9 @@ import com.github.jocull.raftcache.lib.raft.messages.AppendEntries;
 import com.github.jocull.raftcache.lib.raft.messages.VoteRequest;
 import com.github.jocull.raftcache.lib.raft.messages.VoteResponse;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -19,7 +21,6 @@ import java.util.stream.Collectors;
 
 class RaftNodeBehaviorLeader extends RaftNodeBehavior {
     private final int heartbeatTimeoutMillis = 50;
-    private final Executor delayedExecutor;
 
     private CompletableFuture<?> heartbeatTimeout;
 
@@ -35,7 +36,6 @@ class RaftNodeBehaviorLeader extends RaftNodeBehavior {
 
     public RaftNodeBehaviorLeader(RaftNodeImpl self, int term) {
         super(self, NodeStates.LEADER, term);
-        delayedExecutor = CompletableFuture.delayedExecutor(heartbeatTimeoutMillis, TimeUnit.MILLISECONDS, this.self.nodeExecutor);
 
         entriesSubscriber = new EventSubscriber() {
             @Override
@@ -83,49 +83,64 @@ class RaftNodeBehaviorLeader extends RaftNodeBehavior {
     }
 
     private void appendEntries(AppendEntriesTrigger trigger) {
-        synchronized (self.getActiveConnections()) {
-            final TermIndex committedTermIndex = self.logs.getCommittedTermIndex();
-            final com.github.jocull.raftcache.lib.raft.messages.TermIndex committedTermIndexMessage =
-                    new com.github.jocull.raftcache.lib.raft.messages.TermIndex(committedTermIndex.getTerm(), committedTermIndex.getIndex());
+        final TermIndex committedTermIndex = self.logs.getCommittedTermIndex();
+        final com.github.jocull.raftcache.lib.raft.messages.TermIndex committedTermIndexMessage =
+                new com.github.jocull.raftcache.lib.raft.messages.TermIndex(committedTermIndex.getTerm(), committedTermIndex.getIndex());
 
-            // TODO: The connection should be managing its heartbeat timeouts when dealing with continuations.
-            //       For example, one connection might have no logs to send, so it shouldn't reset its heartbeat timeout.
-            //       Another connection might have logs to send, so it is safe to reset the heartbeat timeout for it.
-            self.getActiveConnections().forEach(c -> {
-                if (!c.shouldSendNextEntries(heartbeatTimeoutMillis)) {
-                    LOGGER.debug("{} Too soon to send entries to {} - last send {}, receive {}", self.id, c.getRemoteNodeId(), c.getLastEntriesSent(), c.getLastEntriesAcknowledged());
-                    return;
-                }
+        self.activeConnections.forEach(c -> {
+            if (!c.shouldSendNextEntries(heartbeatTimeoutMillis)) {
+                LOGGER.debug("{} Still waiting for ack before send entries to {} - last send {}, receive {}", self.id, c.getRemoteNodeId(), c.getLastEntriesSent(), c.getLastEntriesAcknowledged());
+                return;
+            }
 
-                final List<AppendEntries.RaftLog> entries = self.logs.getLogRange(c.getTermIndex(), 200, this::transformLog);
-                if (entries.isEmpty() && trigger == AppendEntriesTrigger.CONTINUATION) {
-                    // Continuations only send them if there's entries to send, otherwise they stay quiet
-                    LOGGER.debug("{} No logs remaining to send on continuation to {}", self.getId(), c.getRemoteNodeId());
-                    return;
-                }
+            final List<AppendEntries.RaftLog> entries = self.logs.getLogRange(c.getTermIndex(), 200, this::transformLog);
+            if (entries.isEmpty() && trigger == AppendEntriesTrigger.CONTINUATION) {
+                // Continuations only send them if there's entries to send, otherwise they stay quiet
+                LOGGER.debug("{} No logs remaining to send on continuation to {}", self.getId(), c.getRemoteNodeId());
+                return;
+            }
 
-                if (!entries.isEmpty()) {
-                    final com.github.jocull.raftcache.lib.raft.messages.TermIndex newEndTermIndex = entries.get(entries.size() - 1).getTermIndex();
-                    LOGGER.debug("{} Appending entries to {} setting index {} -> {} with {} entries @ term {}", self.getId(), c.getRemoteNodeId(), c.getTermIndex(), newEndTermIndex, entries.size(), term);
-                } else {
-                    LOGGER.debug("{} Sending heartbeat to {}", self.getId(), c.getRemoteNodeId());
-                }
+            if (!entries.isEmpty()) {
+                final com.github.jocull.raftcache.lib.raft.messages.TermIndex newEndTermIndex = entries.get(entries.size() - 1).getTermIndex();
+                LOGGER.debug("{} Appending entries to {} setting index {} -> {} with {} entries @ term {}", self.getId(), c.getRemoteNodeId(), c.getTermIndex(), newEndTermIndex, entries.size(), term);
+            } else {
+                LOGGER.debug("{} Sending heartbeat to {}", self.getId(), c.getRemoteNodeId());
+            }
 
-                c.sendAppendEntries(new AppendEntries(
-                        term,
-                        new com.github.jocull.raftcache.lib.raft.messages.TermIndex(c.getTermIndex().getTerm(), c.getTermIndex().getIndex()),
-                        committedTermIndexMessage,
-                        entries));
-                c.setLastEntriesSent(Instant.now());
-            });
-        }
+            c.sendAppendEntries(new AppendEntries(
+                    term,
+                    new com.github.jocull.raftcache.lib.raft.messages.TermIndex(c.getTermIndex().getTerm(), c.getTermIndex().getIndex()),
+                    committedTermIndexMessage,
+                    entries));
+            c.setLastEntriesSent(Instant.now());
+        });
 
         // After appending, reset the next heartbeat
         if (heartbeatTimeout != null && !heartbeatTimeout.isDone()) {
             heartbeatTimeout.cancel(false);
         }
+
+        // What should the next timeout be anyway? Find the lowest common denominator.
+        // Needs to target the time that is (lastSent + timeout) from now
+        final int targetTimeoutMillis = self.activeConnections.stream()
+                .map(NodeCommunicationState::getLastEntriesSent)
+                .min(Instant::compareTo)
+                .map(minTime -> {
+                    final Instant targetTime = minTime.plus(heartbeatTimeoutMillis, ChronoUnit.MILLIS);
+                    final long targetMillis = Duration.between(Instant.now(), targetTime).toMillis();
+                    if (targetMillis < 0) {
+                        return 0;
+                    }
+                    if (targetMillis > heartbeatTimeoutMillis) {
+                        return heartbeatTimeoutMillis;
+                    }
+                    return (int) targetMillis;
+                })
+                .orElse(0);
+
+        final Executor delayedExecutor = CompletableFuture.delayedExecutor(targetTimeoutMillis, TimeUnit.MILLISECONDS, this.self.nodeExecutor);
         heartbeatTimeout = CompletableFuture.runAsync(this::onHeartbeatTimeout, delayedExecutor);
-        LOGGER.trace("{} Scheduled next heartbeat ({} ms)", this.self.getId(), heartbeatTimeoutMillis);
+        LOGGER.trace("{} Scheduled next heartbeat ({} ms)", this.self.getId(), targetTimeoutMillis);
     }
 
     private void updateCommittedIndex() {
